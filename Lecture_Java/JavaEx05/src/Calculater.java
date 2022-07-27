0203d719e59cd0d88442bf2384063e86cf8cb385
heejun_kim <khj878@hanmail.net>
2022-07-27 15:33:09 +0900

    Add files via upload

---
+++ Lecture_Java/JavaEx05/src/Calculater.java
@@ -0,0 +1,17 @@
+public class Calculater {
+	int x, y;
+	
+	int Add() {
+		return x + y;
+	}
+	int Minus() {
+		return x - y;
+	}
+	int Mux() {
+		return x * y;
+	}
+	int Div() {
+		return x / y;
+	}
+	
+}
